62485e7c643bcbb60f16816f507c5c71380881f5
Jingu Kim <jingu.kim@gmail.com>
2014-01-26 14:54:15 -0800

    example_nmf_1.m - modified comments

--- example_nmf_1.m
+++ example_nmf_1.m
@@ -1,14 +1,14 @@
-% ------------------------------ Usage examples ------------------------------
+% -------------------------------------------------------------------------
+% Usage examples for nonnegative matrix factorization 
+% -------------------------------------------------------------------------
 m = 300;
 n = 200;
 k = 10;
-
 A = rand(m,n);
 
 % -------------------------------------------------------------------------
 % Comment some of examples and try to execute!
 % -------------------------------------------------------------------------
-
 [W,H,iter,REC]=nmf(A,k,'tol',1e-3,'method','anls_bpp');
 [W,H,iter,REC]=nmf(A,k,'verbose',2,'method','mu','max_iter',1000);
 [W,H,iter,REC]=nmf(A,k,'verbose',1,'method','hals','max_iter',1000);
@@ -25,4 +25,3 @@ for i=1:length(algs)
     % L1-norm regularization test
     [W,H,iter,REC]=nmf(A,k,'tol',1e-3,'method',algs{i},'reg_w',[0.1 0],'reg_h',[0 0.8]);
 end
-
