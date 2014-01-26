dd0f11b0eb064bcf6117b12cf583b6f37edb8b9e
Jingu Kim <jingu.kim@gmail.com>
2014-01-26 14:54:33 -0800

    example_nmf_2.m - modified comments

--- example_nmf_2.m
+++ example_nmf_2.m
@@ -1,8 +1,10 @@
-% ------------------------------ Reconstruction test ------------------------------
+% -------------------------------------------------------------------------
+% Usage examples for nonnegative matrix factorization:
+%   Latent factors reconstruction test
+% -------------------------------------------------------------------------
 m = 300;
 n = 200;
 k = 10;
-
 W_org = rand(m,k);, W_org(rand(m,k)>0.5)=0;
 H_org = rand(k,n);, H_org(rand(k,n)>0.5)=0;
 
@@ -10,17 +12,15 @@ H_org = rand(k,n);, H_org(rand(k,n)>0.5)=0;
 norm2=sqrt(sum(W_org.^2,1));
 toNormalize = norm2>0;
 W_org(:,toNormalize) = W_org(:,toNormalize)./repmat(norm2(toNormalize),m,1);
-
 A = W_org * H_org;
 
-% Try with other methods as well
+% This example is using anls_bpp method, but try with other methods too
 [W,H,iter,HIS]=nmf(A,k,'method','anls_bpp','min_iter',100,'tol',1e-5);
 
-% -------------------- column reordering before computing difference
+% -------------- column reordering before computing difference --------------
 % This reording code is only for test purpose. 
 % Look for Hungarian method (also known as Kuhn–Munkres algorithm)
 % for optimal assignments.
-
 reorder = zeros(k,1);
 selected = zeros(k,1);
 for i=1:k
@@ -44,7 +44,6 @@ end
 
 W_org = W_org(:,reorder);
 H_org = H_org(reorder,:);
-% ---------------------------------------------------------------------    
-
+% ---------------------------------------------------------------------------
 recovery_error_W = norm(W_org-W)/norm(W_org)
 recovery_error_H = norm(H_org-H)/norm(H_org)
